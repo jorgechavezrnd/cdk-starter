ece59fa60c4a8b75d02a55503d2aa6cdfcc3c013
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-04 13:16:45 -0400

    CDK aspects

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -2,9 +2,13 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
   targetBucketArn: photosStack.photosBucketArn
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
